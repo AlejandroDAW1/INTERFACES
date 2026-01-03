b8633efd5678359c0362b683a91856766ec63390
alejandro <alejandrogonzalezaldeguer@gmail.com>
2026-01-03 12:48:38 +0100

    PROYECTO ACABADO

--- BOOTSTRAP/ProyectoBootstrap/componentes1/src/main.js
+++ BOOTSTRAP/ProyectoBootstrap/componentes1/src/main.js
@@ -10,3 +10,22 @@ function lightDark(){
   theme.textContent = esOscuro ? "🌙" : "☀️";
 }
 
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('¡Todo ha salido bien! Esto es una alerta verde', 'success')
+  })
+}
